4120046229024991c60555b6e0511ff5c80caee5
Timothy Moran <tyrsius@gmail.com>
2016-01-27 22:32:26 -0800

    extract navLink

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,28 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Navbar, Nav} from 'react-bootstrap';
 import { Link } from 'react-router';
-
-@connect(state => ({
-    activeUrl: state.routing.location.pathname
-}))
-class NavLink extends Component {
-    render() {
-        return (
-            <li role={'presentation'} className={this.props.activeUrl == this.props.href ? 'active' : ''}>
-                <Link to={this.props.href}>{this.props.label}</Link>
-            </li>
-        );
-    }
-}
+import NavLink from 'components/navLink';
 
 export default class NavBar extends Component {
-    static propTypes = {
-        
-    }
-    constructor(props, ...args) {
-        super(props, ...args);
-        this.state = {};
-    }
 	render() {
 		return (
             <Navbar inverse>
