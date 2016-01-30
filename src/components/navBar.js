a96661cfba50a2285c285a872ceb88648647adee
Timothy Moran <tyrsius@gmail.com>
2016-01-30 13:17:15 -0800

    whitespace test

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { Navbar, Nav, MenuItem, NavDropdown, Glyphicon, Button} from 'react-bootstrap';
 import { Link } from 'react-router';
 import NavLink from 'components/navLink';
-
 import { logout } from 'actions/auth'
 import { toggleCollapse } from 'actions/navigation'
 
